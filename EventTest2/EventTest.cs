1c3c6c148a9e00656e91a6009abb8fa7e2135675
sazuxerox <sazzadhossain@live.com>
2015-12-27 00:40:13 +0600

    Initial Commit

---
+++ EventTest2/EventTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest2
+{
+    public class EventTest
+    {
+        private int _value;
+
+        public delegate void NumManipulationHandler();
+
+        public event NumManipulationHandler ChangeNum;
+
+        public EventTest(int n)
+        {
+            SetValue(n);
+        }
+        protected virtual void OnNumChanged()
+        {
+            if (ChangeNum != null)
+            {
+                ChangeNum();
+            }
+            else
+            {
+                Console.WriteLine("Event fired!");
+            }
+        }
+
+        public void SetValue(int n)
+        {
+            if (_value != n)
+            {
+                _value = n;
+                OnNumChanged();
+            }
+        }
+    }
+}
---
+++ EventTest2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = new EventTest(5);
+            e.SetValue(7);
+            e.SetValue(11);
+            Console.ReadKey();
+        }
+    }
+}
